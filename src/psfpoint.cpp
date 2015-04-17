271dc81fb60aea53a18243216cd8cfe02f425c51
loveemu <loveemu@gmail.com>
2015-04-17 23:00:32 +0900

    Add Windows UTF-8 support again

--- src/psfpoint.cpp
+++ src/psfpoint.cpp
@@ -14,6 +14,10 @@
 #include <limits>
 #include <algorithm>
 
+#ifdef WIN32
+#include <Windows.h>
+#endif
+
 #include "PSFFile.h"
 
 #define APP_NAME    "psfpoint"
@@ -32,6 +36,128 @@ void replace_all(std::string& str, const std::string& from, const std::string& t
 		str.replace(pos, from.length(), to);
 		pos += to.length();
 	}
+}
+
+bool mbs_to_utf8(std::string & utf8str, const std::string & mbstr)
+{
+#ifdef WIN32
+	int ret;
+	int wcs_len;
+	wchar_t * wcsbuf;
+	int utf8_len;
+	char * utf8buf;
+
+	if (mbstr == "") {
+		utf8str = "";
+		return true;
+	}
+
+	wcs_len = MultiByteToWideChar(CP_ACP, 0, &mbstr[0], -1, NULL, 0);
+	if (wcs_len == 0) {
+		return false;
+	}
+
+	wcsbuf = (wchar_t *)malloc(sizeof(wchar_t) * wcs_len);
+	if (wcsbuf == NULL) {
+		return false;
+	}
+
+	ret = MultiByteToWideChar(CP_ACP, 0, &mbstr[0], -1, wcsbuf, wcs_len);
+	if (ret == 0) {
+		free(wcsbuf);
+		return false;
+	}
+
+	utf8_len = WideCharToMultiByte(CP_UTF8, 0, wcsbuf, -1, NULL, 0, NULL, NULL);
+	if (utf8_len == 0) {
+		free(wcsbuf);
+		return false;
+	}
+
+	utf8buf = (char *)malloc(utf8_len);
+	if (utf8buf == NULL) {
+		free(wcsbuf);
+		return false;
+	}
+
+	ret = WideCharToMultiByte(CP_UTF8, 0, wcsbuf, -1, utf8buf, utf8_len, NULL, NULL);
+	if (ret == 0) {
+		free(utf8buf);
+		free(wcsbuf);
+		return false;
+	}
+
+	utf8str = utf8buf;
+	free(utf8buf);
+	free(wcsbuf);
+
+	return true;
+#else
+	// Hopefully, a modern unix OS is using utf-8 and needs no charset conversion.
+	utf8str = mbstr;
+	return true;
+#endif
+}
+
+bool utf8_to_mbs(std::string & mbstr, const std::string & utf8str)
+{
+#ifdef WIN32
+	int ret;
+	int wcs_len;
+	wchar_t * wcsbuf;
+	int mbs_len;
+	char * mbsbuf;
+
+	if (utf8str == "") {
+		mbstr = "";
+		return true;
+	}
+
+	wcs_len = MultiByteToWideChar(CP_UTF8, 0, &utf8str[0], -1, NULL, 0);
+	if (wcs_len == 0) {
+		return false;
+	}
+
+	wcsbuf = (wchar_t *)malloc(sizeof(wchar_t) * wcs_len);
+	if (wcsbuf == NULL) {
+		return false;
+	}
+
+	ret = MultiByteToWideChar(CP_UTF8, 0, &utf8str[0], -1, wcsbuf, wcs_len);
+	if (ret == 0) {
+		free(wcsbuf);
+		return false;
+	}
+
+	mbs_len = WideCharToMultiByte(CP_ACP, 0, wcsbuf, -1, NULL, 0, NULL, NULL);
+	if (mbs_len == 0) {
+		free(wcsbuf);
+		return false;
+	}
+
+	mbsbuf = (char *)malloc(mbs_len);
+	if (mbsbuf == NULL) {
+		free(mbsbuf);
+		return false;
+	}
+
+	ret = WideCharToMultiByte(CP_ACP, 0, wcsbuf, -1, mbsbuf, mbs_len, NULL, NULL);
+	if (ret == 0) {
+		free(mbsbuf);
+		free(wcsbuf);
+		return false;
+	}
+
+	mbstr = mbsbuf;
+	free(mbsbuf);
+	free(wcsbuf);
+
+	return true;
+#else
+	// Hopefully, a modern unix OS is using utf-8 and needs no charset conversion.
+	mbstr = utf8str;
+	return true;
+#endif
 }
 
 static void usage(const char * progname)
@@ -146,10 +272,24 @@ int main(int argc, char *argv[])
 		}
 
 		if (psf_tags.size() != 0) {
+			bool charset_error = false;
 			for (auto itr = psf_tags.begin(); itr != psf_tags.end(); ++itr) {
 				const std::string & name = (*itr).first;
 				const std::string & value = (*itr).second;
-				psf->tags[name] = value;
+
+				std::string value_raw;
+				if (!mbs_to_utf8(value_raw, value)) {
+					charset_error = true;
+					break;
+				}
+				psf->tags[name] = value_raw;
+			}
+
+			if (charset_error) {
+				printf("%s: charset conversion error\n", filename.c_str());
+				num_errors++;
+				delete psf;
+				continue;
 			}
 
 			if (!PSFFile::save(filename, psf->version, &psf->reserved[0], psf->reserved.size(), psf->compressed_exe.compressed_data(), psf->compressed_exe.compressed_size(), psf->tags)) {
@@ -168,7 +308,12 @@ int main(int argc, char *argv[])
 			printf("psfpoint");
 			for (auto itr = current_tags.begin(); itr != current_tags.end(); ++itr) {
 				const std::string & name = (*itr).first;
-				const std::string & value = (*itr).second;
+				const std::string & value_raw = (*itr).second;
+
+				std::string value;
+				if (!utf8_to_mbs(value, value_raw)) {
+					fprintf(stderr, "Warning: Charset conversion error\n");
+				}
 
 				if (name.find_first_of(" ") != std::string::npos || value.find_first_of(" ") != std::string::npos || value.find_first_of("\n") != std::string::npos) {
 					printf(" \"-%s=%s\"", name.c_str(), value.c_str());
